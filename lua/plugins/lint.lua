151db6c1dae43ac387e00f93a604e29cee603a81
Roman Mikhaelian <a.danon93@gmail.com>
2025-06-04 06:03:48 +0300

    adjust linter

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -5,13 +5,9 @@ return {
 		opts = {},
 		config = function()
 			local lint = require("lint")
+			local globalLint = require("lint")
 			lint.linters_by_ft = {
-				javascript = { "cspell" },
-				vue = { "cspell" },
-				javascriptreact = { "cspell" },
-				typescript = { "cspell" },
-				typescriptreact = { "cspell" },
-				nunjucks = { "djlint", "cspell" },
+				nunjucks = { "djlint" },
 			}
 
 			local lint_augroup = vim.api.nvim_create_augroup("linters_by_ft", { clear = true })
@@ -19,11 +15,13 @@ return {
 				group = lint_augroup,
 				callback = function()
 					lint.try_lint()
+					globalLint.try_lint("cspell")
 				end,
 			})
 
 			vim.keymap.set("n", "<leader>l", function()
 				lint.try_lint()
+				globalLint.try_lint("cspell")
 			end)
 		end,
 	},
